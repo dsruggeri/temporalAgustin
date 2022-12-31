caa2365280dc0c803a30e33e96173b75e8234579
diego <dsruggeri@gmail.com>
2022-12-31 19:16:40 -0300

    tercer commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
+let botonPista = document.getElementById('boton-pista');
+let divPista = document.getElementById('divPista')
+botonPista.addEventListener('click', mostrarPista);
+
 function mostrarPista() {
-    cardPista.classList.remove("oculto");    
+    divPista.innerHTML = 
+    `
+    <div class="card text-bg-info mb-3 pista" style="max-width: 18rem;">
+    <div class="card-header">PISTA:</div>
+    <div class="card-body">
+      <h5 class="card-title">¿De verdad necesitás una pista?</h5>
+      <p class="card-text">Esto podría ser un trabajo práctico...</p>
+      <p class="card-text">Ya está, arriesgá...</p>
+    </div>
+  </div>
+    `
+    
+    
 }
 
-let cardPista = document.getElementById('pista')
-let botonPista = document.getElementById('boton-pista');
 
-botonPista.addEventListener('click', mostrarPista());
+
+
 
 
