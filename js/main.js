d8f4a14ffba1404b98c7d9bb8273a480c054a595
diego <dsruggeri@gmail.com>
2022-12-31 18:57:58 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-let respuesta = "";
+function mostrarPista() {
+    cardPista.classList.remove("oculto");    
+}
+
+let cardPista = document.getElementById('pista')
+let botonPista = document.getElementById('boton-pista');
+
+botonPista.addEventListener('click', mostrarPista());
 
 
